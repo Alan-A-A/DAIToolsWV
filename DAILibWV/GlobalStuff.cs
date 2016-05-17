4038006421ebcb3d86ec5ef80fcd61215ff36b8e
Alan A. A. <alan.cpp@outlook.com>
2016-05-17 21:19:47 +0200

    Completed documentation for GlobalStuff.

--- DAILibWV/GlobalStuff.cs
+++ DAILibWV/GlobalStuff.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace DAILibWV
 {
+    /// <summary>
+    /// Static class representing a <see cref="Dictionary{TKey, TValue}"/> with helper methods.
+    /// </summary>
     public static class GlobalStuff
     {
         public static Dictionary<string, string> settings;
 
+        /// <summary>
+        /// Searches the dictionary for an entry with the given key.
+        /// </summary>
+        /// <param name="name">The key to use for search.</param>
+        /// <returns></returns>
         public static string FindSetting(string name)
         {
             foreach (KeyValuePair<string, string> setting in settings)
@@ -18,6 +26,11 @@ namespace DAILibWV
             return "";
         }
 
+        /// <summary>
+        /// Writes the given value using the given key.
+        /// </summary>
+        /// <param name="key">The key to be used.</param>
+        /// <param name="value">The value to be assigned to the given key.</param>
         public static void AssignSetting(string key, string value)
         {
             settings[key] = value;
