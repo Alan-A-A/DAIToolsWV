0b9d3fdb71eee27a8e6f5d480e119f9b37f9a9ed
Alan A. A. <alan.cpp@outlook.com>
2016-05-17 21:15:12 +0200

    Completed documentation for Debug.

--- DAILibWV/Debug.cs
+++ DAILibWV/Debug.cs
@@ -7,6 +7,9 @@ using System.Windows.Forms;
 
 namespace DAILibWV
 {
+    /// <summary>
+    /// Static class for debug output.
+    /// </summary>
     public static class Debug
     {
         [System.Runtime.InteropServices.DllImport("user32.dll")]
@@ -16,11 +19,20 @@ namespace DAILibWV
 
         public static RichTextBox box = null;
 
+        /// <summary>
+        /// Setter method to set the <see cref="RichTextBox"/> member field.
+        /// </summary>
+        /// <param name="rtb">The RichTextBox to use.</param>
         public static void SetBox(RichTextBox rtb)
         {
             box = rtb;
         }
 
+        /// <summary>
+        /// Logs the given string into the <see cref="Debug.box"/>.
+        /// </summary>
+        /// <param name="s">The text to log.</param>
+        /// <param name="update">True to update the window after appending the text.</param>
         public static void Log(string s, bool update = true)
         {
             lock (_sync)
@@ -37,6 +49,9 @@ namespace DAILibWV
             }
         }
 
+        /// <summary>
+        /// Same as <see cref="Log(string, bool)"/> but logs line-wise.
+        /// </summary>
         public static void LogLn(string s, bool update = true)
         {
             Log(s + "\n", update);
